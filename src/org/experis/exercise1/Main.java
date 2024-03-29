40a8eb188c04a08b2e3eebde8100a27eaa359767
luca-masera <lucamasera1995@gmail.com>
2024-03-29 16:22:25 +0100

    Esercizio 1: Java Shop

--- src/org/experis/exercise1/Main.java
+++ src/org/experis/exercise1/Main.java
@@ -2,6 +2,21 @@ package org.experis.exercise1;
 
 public class Main {
     public static void main(String[] args) {
+    Prodotto prodotto1 = new Prodotto("Macchina","Mini cooper", 14000, 22);
+    Prodotto prodotto2 = new Prodotto("Tazza","Tazza orientale di marmo", 150,22);
 
+    //STAMPO prodotto1
+        System.out.println("Il mio primo prodotto è una:" + prodotto1.nomeEsteso());
+        System.out.println("Il modello del prodotto è:" + " " + prodotto1.descrizione);
+        System.out.println("Il prezzo base senza Iva è:" + prodotto1.price() + "$");
+        System.out.println("Il prezzo con Iva è:" + prodotto1.priceIva() + "$");
+
+        System.out.println();
+
+    //STAMPO prodotto2
+        System.out.println("Il mio secondo prodotto è una:" + prodotto2.nomeEsteso());
+        System.out.println("Il modello del prodotto è:" + " " + prodotto2.descrizione);
+        System.out.println("Il prezzo base senza Iva è:" + prodotto2.price() + "$");
+        System.out.println("Il prezzo con Iva è:" + prodotto2.priceIva() + "$");
     }
 }
--- src/org/experis/exercise1/Prodotto.java
+++ src/org/experis/exercise1/Prodotto.java
@@ -1,4 +1,44 @@
 package org.experis.exercise1;
 
+import java.util.Random;
+
 public class Prodotto {
+    //ATTRIBUTI
+    public int codice;
+    public String nome;
+    public String descrizione;
+    public double prezzo;
+    public double iva;
+
+    //COSTRUTTORI
+
+    public Prodotto(String nomeProdotto, String descrizioneProdotto, double prezzoBase, double ivaProdotto){
+        nome = nomeProdotto;
+        codice = random();
+        descrizione = descrizioneProdotto;
+        prezzo = prezzoBase;
+        iva = ivaProdotto;
+
+    }
+
+
+    //METODI
+    public int random(){
+        Random random = new Random();
+        return random.nextInt(100000000);
+    }
+    public double price(){
+       return prezzo;
+    }
+    public double priceIva(){
+        return prezzo * (1 + iva / 100);
+    }
+    public String nomeEsteso() {
+        String nomeCodice = String.format("%08d", codice); // Pad left di 0
+        return nomeCodice + "-" + nome;
+    }
+
+
+
+
 }
